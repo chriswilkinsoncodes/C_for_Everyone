aa6b0ea10f2e36acb874f2aa738334a403db2da3
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2021-05-18 16:33:20 -0400

    formatting

--- fahrenheit.c
+++ fahrenheit.c
@@ -5,13 +5,15 @@
 
 #include <stdio.h>
 
-int main()
+int main(void)
 {
   int fahrenheit, celsius;
 
   printf("Please enter fahrenheit as an integer: ");
   scanf("%d", &fahrenheit);
+ 
   celsius = (fahrenheit - 32)/1.8; //note conversion
   printf("\n%d Fahrenehit is %d Celsius.\n", fahrenheit, celsius);
+ 
   return 0;
 }
