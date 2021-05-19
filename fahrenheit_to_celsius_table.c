a966185365576a9bf73d9c7f0d23ebc0cc973e46
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2021-05-18 22:36:00 -0400

    select method; added C to F conversion

--- fahrenheit_to_celsius_table.c
+++ fahrenheit_to_celsius_table.c
@@ -10,6 +10,13 @@ int main(void)
 {
   float fahr, celsius;
   int lower, upper, step;
+  int method;
+
+  printf("\nEnter conversion method.");
+  printf("\n1. Fahrenheit to Celsius");
+  printf("\n2. Celsius to Fahrenheit");
+  printf("\n(1 or 2): ");
+  scanf("%d", &method);
 
   printf("\nEnter Start of range: ");
   scanf("%d", &lower);
@@ -17,16 +24,34 @@ int main(void)
   scanf("%d", &upper);
   printf("Enter Step value: ");
   scanf("%d", &step);
-  
-  fahr = lower;
-  printf("\nFahr.  Celsius\n");
-  printf("--------------\n");
-  while (fahr <= upper)
-  {
-    celsius = (5.0 / 9.0) * (fahr - 32.0);
-    printf("%5.0f %8.2f\n", fahr, celsius);
-    fahr = fahr + step;
+ 
+  if(method == 1)
+	{ 
+		fahr = lower;
+		printf("\nFahr.  Celsius\n");
+		printf("--------------\n");
+		while (fahr <= upper)
+		{
+			celsius = (5.0 / 9.0) * (fahr - 32.0);
+			printf("%5.0f %8.2f\n", fahr, celsius);
+			fahr = fahr + step;
+		}
+		printf("\n");
   }
-  printf("\n");
-  return 0;
+
+  if(method == 2)
+	{ 
+		celsius = lower;
+		printf("\nCelsius    Fahr.\n");
+		printf("----------------\n");
+		while (celsius <= upper)
+		{
+			fahr = (9.0 / 5.0) * celsius + 32.0;
+			printf("%7.0f %8.2f\n", celsius, fahr);
+			celsius = celsius + step;
+		}
+		printf("\n");
+  }
+
+	return 0;
 }
