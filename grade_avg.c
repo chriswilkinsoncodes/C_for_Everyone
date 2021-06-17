2e26c704f9bbb67a304e396bf0b4dfd213a7910c
chris wilkinson <chriswilkinsoncodes@gmail.com>
2021-06-16 20:31:28 -0400

    initial commit

---
+++ grade_avg.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	const int SIZE = 5;
+
+	int grades[]= {78, 67, 92, 83, 88};
+  double sum = 0.0;
+	int i;
+
+	printf("\nmygrades are: \n");
+
+	for(i = 0; i < SIZE; i++)
+	{
+		printf("%d\t ", grades[i]);
+    sum = sum + grades[i];
+	}
+
+  printf("\n\n");
+  printf("my average is: %.2f\n\n", sum/SIZE);
+
+	return 0;
+}
+	
+
