08c93fec56c8b4c9129370398ef0f916ce2dc59b
chris wilkinson <chriswilkinsoncodes@gmail.com>
2022-11-30 16:21:30 -0500

    defined and used MILE_YARDS const instead of 'magic number'

--- marathon.c
+++ marathon.c
@@ -4,13 +4,15 @@
 
 #include <stdio.h>
 
+#define MILE_YARDS 1760.0
+
 int main()
 {
   int miles = 26, yards = 385;
   float kilometres;
 
   /* use 1760.0 to cast to float; otherwise int/int will return 0; e.g. 385/1760 */
-  kilometres = 1.609 * (miles + yards / 1760.0);
+  kilometres = 1.609 * (miles + yards / MILE_YARDS);
   printf("\nA marathon is %f kilometres.\n\n", kilometres);
   return 0;
 }
